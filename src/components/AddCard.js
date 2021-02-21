29687c91c1c1ad8d2ea7f15b9eb8c61d7fc2c721
ayberkgerey <ayberkgerey@gmail.com>
2021-02-21 13:38:59 +0300

    Addcard style done

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -11,8 +11,8 @@ export default function AddCard() {
   return (
     <View style={styles.container}>
       <TextInput multiline={true} style={styles.inputContainer} />
-      <TouchableOpacity>
-        <Text style={styles.buttonStyle}>Save</Text>
+      <TouchableOpacity style={styles.buttonLayout}>
+        <Text style={styles.buttonStyle}>></Text>
       </TouchableOpacity>
     </View>
   );
@@ -31,13 +31,26 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     borderRadius: 20,
-    width: '91%',
+    width: '85%',
     height: '101%',
     color: '#ffc100',
     justifyContent: 'flex-start',
   },
   buttonStyle: {
     borderRadius: 30,
-    color: 'green',
+    color: '#171717',
+    fontWeight: 'bold',
+    fontSize: 90,
+    marginBottom: 15,
+  },
+  buttonLayout: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '16%',
+    backgroundColor: '#ffc100',
+    borderBottomRightRadius: 19,
+    borderTopRightRadius: 19,
+
   },
 });
